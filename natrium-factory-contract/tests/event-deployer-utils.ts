5601cbb676d8086e2ab0fa4b0c77b857e72cb444
Usama <usamabinqasim1234@gmail.com>
2024-12-19 12:04:18 +0500

    Initialize subgraph

---
+++ natrium-factory-contract/tests/event-deployer-utils.ts
@@ -0,0 +1,43 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
+import {
+  Initialized,
+  OwnershipTransferred
+} from "../generated/EventDeployer/EventDeployer"
+
+export function createInitializedEvent(version: BigInt): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(version)
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent =
+    changetype<OwnershipTransferred>(newMockEvent())
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
